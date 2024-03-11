47cd3d3eb4a08ae291a7c145cf049f8adbb139c6
Latica Škubonja <skubonjal@gmail.com>
2024-03-11 19:35:43 +0100

    initial react webshop commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Header } from './components/Header/Header';
+import { MainComponent } from './components/MainComponent/MainComponent';
+import { Footer } from './components/Footer/Footer';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>Hello WebShop2</h1>
+      <Header />
+      <MainComponent />
+      <FooterComponent />          
+    </>
+  )
+}
+
+export default App
---
+++ src/components/MainComponent/MainComponent.jsx
@@ -0,0 +1,10 @@
+function MainComponent(){
+    return(
+        <div>
+            <h2>Main Component</h2>
+            <p>Here will be the list od products</p>
+        </div>
+    )
+}
+
+export{MainComponent}
