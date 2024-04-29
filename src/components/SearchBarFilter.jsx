8f266ad804bf0ff933cc112f60fcd770ac398656
Latica Škubonja <skubonjal@gmail.com>
2024-04-29 22:56:13 +0200

    searchbar

--- src/components/SearchBarFilter.jsx
+++ src/components/SearchBarFilter.jsx
@@ -1,16 +1,33 @@
 import React, { useState } from "react";
 import data from "./databooks.json";
+import { InputGroup, Form } from "react-bootstrap";
 
 function SearchBarFilter() {
+  const [search, setSearch] = useState("");
   return (
     <>
-      <input type="search" />
-      <ul>
-        {filteredData.map((item) => (
-          <li key={item.id}>
-            {item.title}, {item.author}
-          </li>
-        ))}
+      <Form>
+        <InputGroup className="my-3">
+          <Form.Control
+            className="mx-3 py-3"
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder="Search"
+          />
+        </InputGroup>
+      </Form>
+      <ul style={{ listStyle: "none" }}>
+        {data
+          .filter((item) => {
+            return search.toLowerCase() === ""
+              ? null
+              : item.title.toLowerCase().includes(search) ||
+                  item.author.toLowerCase().includes(search);
+          })
+          .map((item) => (
+            <li key={item.id}>
+              {item.title} / {item.author}
+            </li>
+          ))}
       </ul>
     </>
   );
